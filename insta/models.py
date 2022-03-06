7aa8fd15bfd7a1ab6566524fe25d3dae92413747
Esther-Anyona <jkemuntoe@gmail.com>
2022-03-06 16:18:07 +0300

    Add image and profile models

--- insta/models.py
+++ insta/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import datetime as dt
 
 # Create your models here.
+class Image(models.Model):
+    image = models.ImageField(upload_to='ishots/')
+    image_name = models.CharField(max_length=50)
+    image_caption = models.TextField()
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    likes = models.IntegerField(default=0)
+    comments = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.image_name
+
+
+
+class Profile(models.Model):
+    prof_photo = models.ImageField(upload_to='ishots/')
+    bio = models.TextField()
